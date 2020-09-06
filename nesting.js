20266ed8464b919f6edff08924831673ae9ccb8b
justin <justinpolacek@gmail.com>
2020-09-06 16:22:41 -0700

    finished homework

--- nesting.js
+++ nesting.js
@@ -120,8 +120,11 @@ var cat = {
 */
 
 //Code Here
-var grumpyActivity;
-var fluffy2ndFriend;
+var grumpyActivity = cat.catFriends[0].activities[1]
+
+var fluffy2ndFriend = cat.catFriends[1].name
+
+
 
 
 
@@ -162,6 +165,11 @@ var myCar = {
 */
 
 //Code Here
+function recordCleaner() {
+  for(let i = 0; i < myCar.accidents.length; i++){
+    myCar.accidents[i].atFaultForAccident = false 
+  }
+}
 
 
 
@@ -182,4 +190,17 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
 
 //Code Here
 
+function looper() {
+  for(let i = 0; i < numsArr.length; i++) {
+    for(let j = 0; j < numsArr[i].length; j++) {
+      if(numsArr[i][j] % 2 === 0) {
+        numsArr[i][j] = 'even' 
+      }
+      else {
+        numsArr[i][j] = 'odd'
+      }  
+    }
+  }
+  return numsArr
+}
 
