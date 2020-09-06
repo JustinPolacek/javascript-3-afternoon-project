457bf6ff68a488724df3eaae197ce71d5cc7b38e
justin <justinpolacek@gmail.com>
2020-09-06 09:41:08 -0700

    finished destructuing

--- destructuring.js
+++ destructuring.js
@@ -23,7 +23,7 @@ var carDetails = {
 
 //Code Here
 
-
+let { color, make, model, year} = carDetails;
 
 ////////// PROBLEM 2 //////////
 
@@ -34,7 +34,9 @@ var carDetails = {
 */
 
 function greeting( obj ) {
-  //Code Here
+  
+  let { firstName, lastName, title} = obj;
+  
   
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
@@ -55,6 +57,13 @@ function greeting( obj ) {
 
 //Code Here
 
+function totalPopulation(obj){
+  let {utah, california, texas, arizona} = obj
+  return utah + california + texas + arizona
+}
+
+
+
 
 
 ////////// PROBLEM 4 //////////
@@ -68,6 +77,15 @@ function greeting( obj ) {
 */
 
 //Code Here
+
+function ingredients(obj){
+  let {carb, fat, protein} = obj
+  let strArr = []
+  strArr.push(carb)
+  strArr.push(fat)
+  strArr.push(protein)
+  return strArr
+}
 
 
 
@@ -87,6 +105,17 @@ function greeting( obj ) {
 
 //Code Here
 
+function largeNumbers( {first, second, third} ) {
+  if(first < second && first < third) {
+    return first
+  }else if (second < first && second <third){
+    return second; 
+  } else {
+    return third;
+  }
+  }
+
+
 
 
 ////////// PROBLEM 6 //////////
@@ -99,4 +128,16 @@ function greeting( obj ) {
 
 //Code Here
 
+function numberGroups({ a, b, c}){
+  if(a.length > b.length && a.length > c.length){
+    return a;
+  }
+  else if(b.length > a.length && b.length > c.length){
+    return b;
+  }
+  else{
+    return c;
+  }
 
+
+}
